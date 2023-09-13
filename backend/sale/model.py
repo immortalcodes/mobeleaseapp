c8a8ba77384fe23d1bc8fa725be082a193a96979
immortalcodes <21112002mj@gmail.com>
2023-09-14 03:36:55 +0530

    made changes to model

--- backend/sale/model.py
+++ backend/sale/model.py
@@ -1,2 +1,25 @@
 from pydantic import BaseModel
 from typing import Optional, Union , List
+
+
+
+class saleobject(BaseModel):
+    type : str
+    customername : Optional[str] = None
+    customeridimage : Optional[bytes] = None
+    phoneno : Optional[str] = None
+    language : Optional[str] = None
+    unit : Optional[str] = None
+    farm : Optional[str] = None
+    itemarray : dict
+    remark : Optional[str] = None
+    paymentalert : int
+
+
+class farm(BaseModel):
+    farmname : str
+
+
+class farmunit(BaseModel):
+    farmid : int
+    unitname : str
