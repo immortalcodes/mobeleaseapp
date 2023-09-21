ce99fd4c5109e29ee65e1bd28205d53eac28b471
immortalcodes <21112002mj@gmail.com>
2023-09-22 02:55:31 +0530

    added remark model

--- backend/employee/init.py
+++ backend/employee/init.py
@@ -1,6 +1,9 @@
 import requests
 from .utils import *
 from .model import *
+from zoneinfo import ZoneInfo
+from datetime import datetime
+from dotenv import dotenv_values
 
 from fastapi import APIRouter,status,Response, Cookie
 from typing import Union
@@ -8,6 +11,8 @@ from authentication.utils import decodeToken
 from dbconnect import connection, cursor
 
 empRouter = APIRouter()
+config = dotenv_values(".env")
+
 
 
 @empRouter.post("/allemployee", status_code=200)
@@ -135,3 +140,77 @@ async def editEmployee(item:employee,response: Response,access_token: Union[str,
         return {"message":"error in verifying token and permission"}
      
 
+@empRouter.post("/addremark", status_code=200)
+async def addRemark(item:remark,response: Response,access_token: Union[str, None] = Cookie(default=None)):
+    token = decodeToken(access_token)   
+    if token and (token['role'] == 'admin'):
+        current_dt = datetime.now(tz=ZoneInfo(config["TimeZone"]))
+        cursor.execute("INSERT into remark(employeeid,remark,initiater,remarktimestamp) VALUES (%s,%s,%s,%s)",(item.empid,item.remark,'admin',current_dt))
+        response.status_code = status.HTTP_200_OK
+        return {'data':'Remark Added Successfully'}
+    elif token and (token['role'] == 'employee'):
+        empid = token['empid']
+        current_dt = datetime.now(tz=ZoneInfo(config["TimeZone"]))
+        cursor.execute("INSERT into remark(employeeid,remark,initiater,remarktimestamp) VALUES (%s,%s,%s,%s)",(empid,item.remark,'employee',current_dt))
+        response.status_code = status.HTTP_200_OK
+        return {'data':'Remark Added Successfully'}
+    else:
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return {"message":"error in verifying token and permission"}
+    
+
+@empRouter.post("/addreply", status_code=200)
+async def addRemark(item:reply,response: Response,access_token: Union[str, None] = Cookie(default=None)):
+    token = decodeToken(access_token)   
+    if token and (token['role'] == 'admin'):
+        current_dt = datetime.now(tz=ZoneInfo(config["TimeZone"]))
+        cursor.execute("UPDATE  remark SET reply= %s , replytimestamp=%s where remarkid = %s and initiater = 'employee'",(item.reply,current_dt,item.remarkid))
+        response.status_code = status.HTTP_200_OK
+        return {'data':'Reply Added Successfully'}
+    elif token and (token['role'] == 'employee'):
+        empid = token['empid']
+        current_dt = datetime.now(tz=ZoneInfo(config["TimeZone"]))
+        cursor.execute("UPDATE  remark SET reply= %s , replytimestamp=%s where remarkid = %s and initiater = 'admin' and employeeid = %s",(item.reply,current_dt,item.remarkid,empid))
+        response.status_code = status.HTTP_200_OK
+        return {'data':'Reply Added Successfully'}
+    else:
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return {"message":"error in verifying token and permission"}
+
+
+
+@empRouter.post("/viewremarks", status_code=200)
+async def viewremarks(item:employeeid,response: Response,access_token: Union[str, None] = Cookie(default=None)):
+    token = decodeToken(access_token)   
+    if token and (token['role'] == 'admin'):
+        cursor.execute("SELECT * from remark where employeeid = %s ORDER BY remarktimestamp",(item.empid,))
+        all_remarks = cursor.fetchall()
+        data = {}
+        for remark in all_remarks:
+            data[remark[0]] = {
+                'remark':remark[2],
+                'reply':remark[3],
+                'initiater':remark[4],
+                'remarktimestamp':remark[5],
+                'replytimestamp':remark[6],
+            }
+        response.status_code = status.HTTP_200_OK
+        return {'data':data}
+    elif token and (token['role'] == 'employee'):
+        empid = token['empid']
+        cursor.execute("SELECT * from remark where employeeid = %s ORDER BY remarktimestamp",(empid,))
+        all_remarks = cursor.fetchall()
+        data = {}
+        for remark in all_remarks:
+            data[remark[0]] = {
+                'remark':remark[2],
+                'reply':remark[3],
+                'initiater':remark[4],
+                'remarktimestamp':remark[5],
+                'replytimestamp':remark[6],
+            }
+        response.status_code = status.HTTP_200_OK
+        return {'data':data}
+    else:
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return {"message":"error in verifying token and permission"}
