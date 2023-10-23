384c5fa14d7b6c9b0a7b8c3f4e696f53d251818c
tusharb <tusharbansal@iitbhilai.ac.in>
2023-10-24 00:01:24 +0530

    added baseUrl for IOS

--- client/lib/globals.dart
+++ client/lib/globals.dart
@@ -1,3 +1,8 @@
 library globals;
 
-String baseUrl = "http://10.0.2.2:8000";
+
+//android baseUrl
+// String baseUrl = "http://10.0.2.2:8000";
+
+// ios baseUrl
+String baseUrl = "http://127.0.0.1:8000";
